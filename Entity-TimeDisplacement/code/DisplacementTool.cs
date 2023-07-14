bbd160d2c94b819f066f337487e19ff9871a5e6f
Fox <t1758219@gmail.com>
2023-07-14 10:52:50 +0100

    Tracking Displacement Entity -> Displacement Tool, and add Tool option

---
+++ Entity-TimeDisplacement/code/DisplacementTool.cs
@@ -0,0 +1,38 @@
+﻿using Sandbox.Components;
+using Sandbox.Tools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tracking;
+
+namespace Sandbox
+{
+    [Library("tool_displacement", Title = "Displacement", Description = "View a displacement of the entity by 5 seconds.", Group = "fun")]
+    public class DisplacementTool : BaseTool
+    {
+        public override void Simulate()
+        {
+            if (!Game.IsServer)
+                return;
+
+            using (Prediction.Off())
+            {
+                var tr = DoTrace();
+
+                if (!tr.Hit || !tr.Entity.IsValid())
+                    return;
+
+
+                if (tr.Entity is not ModelEntity modelEnt)
+                    return;
+
+                modelEnt.Components.Add(new TrackingDisplacementEntityComponent());
+            }
+
+            base.Simulate();
+        }
+
+    }
+}
